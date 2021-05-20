ce0be54688f405702ad09680d4a525026c815f85
ItsSkyWasTaken <62679043+ItsSkyWasTaken@users.noreply.github.com>
2021-05-20 09:46:11 -0500

    Add a "stop" command to the second task
    Added the option to cancel the second task early by typing "stop" in the command prompt.

--- src/net/sky/simulator/Simulator.java
+++ src/net/sky/simulator/Simulator.java
@@ -1,9 +1,11 @@
 package net.sky.simulator;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Simulator {
     private static final Random r = new Random();
+    private static final Scanner sc = new Scanner(System.in);
 
     private static void runTrial(int numerator, int denominator, int trials) {
         int successes = 0;
@@ -13,11 +15,20 @@ public class Simulator {
             }
         }
         System.out.println("There were " + successes + " successes out of " + trials + " trials observed.");
+        System.exit(0);
     }
 
     private static void countSets(int numerator, int denominator, int trials, int goal) {
         int successes;
         long sets = 0;
+
+        new Thread(() -> {
+            System.out.println("Started the trials!  Type \"stop\" to end the process early.");
+            while(!Simulator.sc.nextLine().equalsIgnoreCase("stop")) {
+                // Do nothing.  Keep taking responses until there is a match.
+            }
+            System.exit(0);
+        }).start();
 
         long lastTime = System.currentTimeMillis();
         long sinceLastTime;
@@ -39,6 +50,7 @@ public class Simulator {
         } while(successes < goal);
 
         System.out.println("It took " + sets + " sets of trials to get " + goal + " successes in " + trials + " trials.");
+        System.exit(0);
     }
 
     public static void main(String[] args) {
